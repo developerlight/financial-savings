65777fe717d4192af42841c30ab0ff0e530b03c1
Samsul Muhyidin <128915331+developerlight@users.noreply.github.com>
2024-02-12 22:00:40 +0700

    feat app listen

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,18 +1,39 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    username: { 
+        type: String, 
+        unique: true, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        unique: true, 
+        required: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
     tabunganId: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Tabungan"
         }
     ],
-    address: { type: String, required: true },
-    phone: { type: String, unique: true, required: true },
-    joined_at: { type: Date, default: Date.now }
+    address: { 
+        type: String, 
+        required: true 
+    },
+    phone: { 
+        type: String, 
+        unique: true, 
+        required: true 
+    },
+    joined_at: { 
+        type: Date, 
+        default: Date.now 
+    }
 });
 
 const User = mongoose.model("User", userSchema);
